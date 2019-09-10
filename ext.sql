de07b5d126d07d1594a63c0e4d9dbb0fc845ee63
juanmacalvo <48252840+juanmacalvo@users.noreply.github.com>
2019-09-10 07:57:04 +0200

    Add files via upload

---
+++ ext.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 10-09-2019 a las 07:54:03
+-- Versión del servidor: 10.1.38-MariaDB
+-- Versión de PHP: 7.1.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `ext`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `course_mapping`
+--
+
+CREATE TABLE `course_mapping` (
+  `saml_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `course_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `blocked` int(1) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `course_mapping`
+--
+
+INSERT INTO `course_mapping` (`saml_id`, `course_id`, `blocked`, `id`) VALUES
+('444444', '9', 0, 3);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `course_mapping`
+--
+ALTER TABLE `course_mapping`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `course_id` (`course_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `course_mapping`
+--
+ALTER TABLE `course_mapping`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
